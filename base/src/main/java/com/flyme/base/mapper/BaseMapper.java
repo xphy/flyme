809ab8e38dbf847583bf33ba052247002e40bde4
xphy <2424919447@qq.com>
2022-12-29 17:37:38 +0800

    原始版父子多模块项目-20221229

---
+++ base/src/main/java/com/flyme/base/mapper/BaseMapper.java
@@ -0,0 +1,15 @@
+package com.flyme.base.mapper;
+
+
+/**
+ * @author ：mmzs
+ * @date ：Created in 2020/4/18 15:21
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+/*
+
+public interface BaseMapper<T> extends Mapper<T>, MySqlMapper<T> {
+}
+*/
