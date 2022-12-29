b3da6c6727bab87b6b8d87bdbd02126cec93464f
xphy <2424919447@qq.com>
2022-12-29 21:07:13 +0800

    父子多模块后端项目包含工具类-20221229

---
+++ basecommon/src/main/java/com/flyme/basecommon/mapper/BaseMapper.java
@@ -0,0 +1,17 @@
+package com.flyme.basecommon.mapper;
+
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * @author ：mmzs
+ * @date ：Created in 2020/4/18 15:21
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+
+public interface BaseMapper<T> extends Mapper<T>, MySqlMapper<T> {
+}
+
